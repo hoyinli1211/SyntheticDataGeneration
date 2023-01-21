1f90db7e61f98bb7d3661eab5d308e07da866878
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-21 21:43:23 +0800

    Update App.py

--- App.py
+++ App.py
@@ -39,7 +39,7 @@ data_type_mapping = {
   #transaction related
   "tran_date": fake.date_this_decade,
   "tran_datetime": fake.date_time,
-  "tran_amount": random.randint(1, 100000),
+  "tran_amount": lambda: random.randint(1, 100000),
   "tran_CD": fake.random_element(elements=("debit", "credit")),
   "tran_status": fake.random_element(elements=("approved", "declined", "pending")),
   "tran_type": fake.random_element(elements=("ATM", "FPS", "CHATs", "SWIFT", "CHEQUE", "Others")),
